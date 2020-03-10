ca96d676abde1d1317cde80ed6315c9e22ba76da
Lizethgutierrez <lizethgutierrrez@gmail.com>
2020-03-10 12:23:20 +0000

    no modules

---
+++ index.js
@@ -0,0 +1,8 @@
+const cool = require("cool-ascii-faces");
+const express = require("express");
+var app = express();
+app.get("/cool",(request,response) => {
+response.send("<html>"+cool()+"</html>");
+});
+app.listen(80);
+console.log("server ready");
